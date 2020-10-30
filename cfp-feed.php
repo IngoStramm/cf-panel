c3007ec28f2f90a9bf4dac2ceb308d0eb8f5477a
Ingo Stramm <ingostramm@gmail.com>
2020-10-30 16:49:18 -0300

    custom feed output

--- cfp-feed.php
+++ cfp-feed.php
@@ -1,49 +1,21 @@
 <?php
 
 /**
- * Registers our custom feed
+ * Deal with the custom RSS templates.
  */
-function cfp_register()
-{
-    add_feed('rss2-with-featured-images', __NAMESPACE__ . '\cfp_generate_content');
-}
-add_action('init', __NAMESPACE__ . '\cfp_register');
-
-/**
- * Generates the content of our custom feed
- */
-function cfp_generate_content()
+function cfp_rss($for_comments)
 {
 
-    add_filter('the_content_feed', __NAMESPACE__ . '\cfp_prepend_thumbnail');
-    add_filter('the_excerpt_rss',  __NAMESPACE__ . '\cfp_prepend_thumbnail');
-
-    if (file_exists(ABSPATH . WPINC . '/feed-rss2.php')) {
-        require(ABSPATH . WPINC . '/feed-rss2.php');
+    if ('widget' === get_query_var('post_type')) {
+        include_once 'feed-templates/feed-widget.php';
+    } else if ('notice' === get_query_var('post_type')) {
+        include_once 'feed-templates/feed-notice.php';
+    } else if ($for_comments) {
+        load_template(ABSPATH . WPINC . '/feed-rss2-comments.php');
+    } else {
+        load_template(ABSPATH . WPINC . '/feed-rss2.php');
     }
-
-    remove_filter('the_content_feed', __NAMESPACE__ . '\cfp_prepend_thumbnail');
-    remove_filter('the_excerpt_rss',  __NAMESPACE__ . '\cfp_prepend_thumbnail');
 }
+remove_all_actions('do_feed_rss2');
 
-/**
- * Prepends the post's featured image to the feed content
- *
- * @param string $content The feed content.
- *
- * @return string The filtered content.
- */
-function cfp_prepend_thumbnail($content)
-{
-
-    if (!has_post_thumbnail()) {
-        return $content;
-    }
-
-    $thumbnail_html = sprintf(
-        "<p>%s</p>\n",
-        get_the_post_thumbnail()
-    );
-
-    return $thumbnail_html . $content;
-}
+add_action('do_feed_rss2', 'cfp_rss', 11);
